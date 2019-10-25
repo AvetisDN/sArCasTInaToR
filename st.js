6e610c721cd0c41e553da794f75df507d07aff1d
AvetisDN <avetisdn@gmail.com>
2019-10-25 20:20:43 +0300

    first commit

---
+++ st.js
@@ -0,0 +1,42 @@
+window.onload = function () {
+    let k = 0;
+    let n = 0;
+    let clipboard = new ClipboardJS('#copy');
+    document.getElementById('src').onkeyup = function (e) {
+        if(/^[a-zA-Zа-яА-ЯЁё]$/.test(e.key)) {
+            document.getElementById('dest').value = sArCasTInaTioN(this.value);
+        }
+        if (this.value.length > 0) {
+            document.getElementById('copy').removeAttribute('disabled');
+        } else {
+            document.getElementById('copy').setAttribute('disabled', 'disabled');
+        }
+    };
+    function sArCasTInaTioN(text) {
+        let result = '';
+        [...text].forEach(c => {
+            let char = c;
+            let rand = Math.round(Math.random());
+            if (rand) k++;
+            else n++;
+            if (k > 2) {
+                k = 0;
+                rand = 0;
+            }
+            if (n > 2) {
+                n = 0;
+                rand = 1;
+            }
+            result += rand ? char.toUpperCase() : char.toLowerCase();
+        });
+        return result;
+    }
+    clipboard.on('success', function(e) {
+        document.getElementById('copied').classList.add('show');
+        setTimeout(function () {
+            document.getElementById('copied').classList.remove('show');
+        }, 2000);
+        e.clearSelection();
+    });
+
+};
